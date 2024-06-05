95c849a95cbe1f4db49221fdc6815a05f9ed11dc
elyse502 <elyseniyibizi502@gmail.com>
2024-06-05 16:17:57 +0200

    Log Users in

--- auth/login.php
+++ auth/login.php
@@ -1,5 +1,44 @@
 <?php require "../includes/header.php"; ?>
 <?php require "../config/config.php"; ?>
+<?php 
+
+
+  
+  if(isset($_POST['submit'])){
+
+    if(empty($_POST['email']) OR empty($_POST['password'])){
+      echo "<script>alert('one or more inputs are empty');</script>";
+    } else {
+
+      $email = $_POST['email'];
+      $password = $_POST['password'];
+
+      
+      //write a query to check for email
+
+      $login = $conn->query("SELECT * FROM users WHERE email = '$email'");
+      $login->execute();
+
+      $fetch = $login->fetch(PDO::FETCH_ASSOC);
+
+      if($login->rowCount() > 0){
+
+        if(password_verify($password, $fetch['password'])){
+          //start session
+          header("location: ".APPURL."");
+        } else {
+          echo "<script>alert('email or password is wrong');</script>";
+
+        }
+      } else {
+        echo "<script>alert('email or password is wrong');</script>";
+
+      }
+    }
+  }  
+
+
+?>
 
     <section class="home-slider owl-carousel">
 
@@ -22,27 +61,27 @@
       <div class="container">
         <div class="row">
           <div class="col-md-12 ftco-animate">
-			<form action="#" class="billing-form ftco-bg-dark p-3 p-md-5">
+			<form action="login.php" method="POST" class="billing-form ftco-bg-dark p-3 p-md-5">
 				<h3 class="mb-4 billing-heading">Login</h3>
 	          	<div class="row align-items-end">
 	          		<div class="col-md-12">
 	                <div class="form-group">
 	                	<label for="Email">Email</label>
-	                  <input type="text" class="form-control" placeholder="Email">
+	                  <input name="email" type="text" class="form-control" placeholder="Email">
 	                </div>
 	              </div>
                  
 	              <div class="col-md-12">
 	                <div class="form-group">
 	                	<label for="Password">Password</label>
-	                    <input type="password" class="form-control" placeholder="Password">
+	                    <input name="password" type="password" class="form-control" placeholder="Password">
 	                </div>
 
                 </div>
                 <div class="col-md-12">
                 	<div class="form-group mt-4">
 							<div class="radio">
-                                <button class="btn btn-primary py-3 px-4">Login</button>
+                      <button type="submit" name="submit" class="btn btn-primary py-3 px-4">Login</button>
 						    </div>
 					</div>
                 </div>

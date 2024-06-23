c80bf451865e4693225751770449ca3b06f3e044
elyse502 <elyseniyibizi502@gmail.com>
2024-06-23 20:37:42 +0200

    Make Home Page Dynamic

---
+++ admin-panel/index.php
@@ -0,0 +1,47 @@
+<?php require "layouts/header.php"; ?>
+
+            
+      <div class="row">
+        <div class="col-md-3">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Products</h5>
+              <!-- <h6 class="card-subtitle mb-2 text-muted">Bootstrap 4.0.0 Snippet by pradeep330</h6> -->
+              <p class="card-text">number of products: 8</p>
+             
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Orders</h5>
+              
+              <p class="card-text">number of orders: 4</p>
+              
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Bookings</h5>
+              
+              <p class="card-text">number of bookings: 4</p>
+              
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Admins</h5>
+              
+              <p class="card-text">number of admins: 3</p>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+     
+<?php require "layouts/footer.php"; ?>

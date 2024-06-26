de4d11d609d3c06c2593c22872bd03d79d334b99
elyse502 <elyseniyibizi502@gmail.com>
2024-06-26 11:35:08 +0200

    Add Starting Session and Validating Functionalities

--- admin-panel/layouts/header.php
+++ admin-panel/layouts/header.php
@@ -1,10 +1,10 @@
 <?php 
 
+    session_start();
     define("ADMINURL", "http://localhost/coffee-blend/admin-panel/");
 
 
-?>
-<!DOCTYPE html>
+?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -12,7 +12,7 @@
     <title>Admin Panel</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
-     <link href="styles/style.css" rel="stylesheet">
+     <link href="<?php echo ADMINURL; ?>/styles/style.css" rel="stylesheet">
     <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </head>
@@ -27,9 +27,10 @@
       </button>
 
       <div class="collapse navbar-collapse" id="navbarText">
+        <?php if(isset($_SESSION['admin_name'])) : ?>
         <ul class="navbar-nav side-nav" >
           <li class="nav-item">
-            <a class="nav-link" style="margin-left: 20px;" href="index.html">Home
+            <a class="nav-link" style="margin-left: 20px;" href="<?php echo ADMINURL; ?>">Home
               <span class="sr-only">(current)</span>
             </a>
           </li>
@@ -46,24 +47,30 @@
             <a class="nav-link" href="bookings-admins/show-bookings.html" style="margin-left: 20px;">Bookings</a>
           </li>
         </ul>
+        <?php endif; ?>
         <ul class="navbar-nav ml-md-auto d-md-flex">
-          <li class="nav-item">
-            <a class="nav-link" href="index.html">Home
-              <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="admins/login-admins.html">login
-            </a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link  dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              username
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Logout</a>
-              
-          </li>
+          <?php if(isset($_SESSION['admin_name'])) : ?>
+            <li class="nav-item">
+              <a class="nav-link" href="<?php echo ADMINURL; ?>">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            
+            <li class="nav-item dropdown">
+              <a class="nav-link  dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <?php echo $_SESSION['admin_name']; ?>
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="<?php echo ADMINURL; ?>/admins/logout.php">Logout</a>
+                
+            </li>
+          <?php else : ?>
+
+            <li class="nav-item">
+              <a class="nav-link" href="<?php echo ADMINURL; ?>/admins/login-admins.php">login
+              </a>
+            </li>
+          <?php endif; ?>
                           
           
         </ul>

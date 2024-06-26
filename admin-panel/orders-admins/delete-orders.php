416aa47d2a4da05aaca23dcf5b59203f2a0163b2
elyse502 <elyseniyibizi502@gmail.com>
2024-06-26 21:04:28 +0200

    Implement Deleting Orders Functionality

---
+++ admin-panel/orders-admins/delete-orders.php
@@ -0,0 +1,19 @@
+<?php require "../layouts/header.php"; ?>
+<?php require "../../config/config.php"; ?>
+<?php 
+
+    if(!isset($_SESSION['admin_name'])){
+        header("location: ".ADMINURL."/admins/login-admins.php");
+    }
+    
+
+    if(isset($_GET['id'])){
+
+        $id = $_GET['id'];
+        
+        
+        $delete = $conn->query("DELETE FROM orders WHERE id='$id'");
+        $delete->execute();
+
+        header("location: show-orders.php");
+    }

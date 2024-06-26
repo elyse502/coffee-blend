3e4260f102eb3e5a557443025453329dbd866925
elyse502 <elyseniyibizi502@gmail.com>
2024-06-26 14:05:13 +0200

    Add Creating New Admins Functionality

--- admin-panel/admins/admins.php
+++ admin-panel/admins/admins.php
@@ -1,6 +1,10 @@
 <?php require "../layouts/header.php"; ?>
 <?php require "../../config/config.php"; ?>
-<?php 
+<?php
+
+  if(!isset($_SESSION['admin_name'])){
+    header("location: ".ADMINURL."/admins/login-admins.php");
+  }
 
   $admins = $conn->query("SELECT * FROM admins");
   $admins->execute();
--- admin-panel/admins/create-admins.php
+++ admin-panel/admins/create-admins.php
@@ -1,7 +1,12 @@
 
 <?php require "../layouts/header.php"; ?>
 <?php require "../../config/config.php"; ?>
-<?php 
+<?php
+
+  if(!isset($_SESSION['admin_name'])){
+    header("location: ".ADMINURL."/admins/login-admins.php");
+  }
+  
 
   if(isset($_POST['submit'])){
 

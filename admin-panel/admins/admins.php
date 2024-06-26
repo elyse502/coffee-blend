01f65a1e3ef9a6995b1c4454553bafc2af079f24
elyse502 <elyseniyibizi502@gmail.com>
2024-06-26 13:03:49 +0200

    Implement Showing Admins Feature

---
+++ admin-panel/admins/admins.php
@@ -0,0 +1,44 @@
+<?php require "../layouts/header.php"; ?>
+<?php require "../../config/config.php"; ?>
+<?php 
+
+  $admins = $conn->query("SELECT * FROM admins");
+  $admins->execute();
+
+  $allAdmins = $admins->fetchAll(PDO::FETCH_OBJ);
+
+?>
+      <div class="row">
+        <div class="col">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title mb-4 d-inline">Admins</h5>
+             <a  href="create-admins.html" class="btn btn-primary mb-4 text-center float-right">Create Admins</a>
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">admin_name</th>
+                    <th scope="col">email</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php foreach($allAdmins as $admin) : ?>
+                  <tr>
+                    <th scope="row"><?php echo $admin->id; ?></th>
+                    <td><?php echo $admin->adminname; ?></td>
+                    <td><?php echo $admin->email; ?></td>
+                   
+                  </tr>
+                  <?php endforeach; ?>
+                  
+                </tbody>
+              </table> 
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+<?php require "../layouts/footer.php"; ?>
--- admin-panel/layouts/header.php
+++ admin-panel/layouts/header.php
@@ -35,7 +35,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="admins/admins.html" style="margin-left: 20px;">Admins</a>
+            <a class="nav-link" href="admins/admins.php" style="margin-left: 20px;">Admins</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="orders-admins/show-orders.html" style="margin-left: 20px;">Orders</a>

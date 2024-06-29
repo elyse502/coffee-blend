3bc57b2634c38efaba8e5fc78103ece6b1032b43
elyse502 <elyseniyibizi502@gmail.com>
2024-06-29 17:20:53 +0200

    Add Deleting Products with Images Feature

---
+++ admin-panel/products-admins/delete-products.php
@@ -0,0 +1,27 @@
+<?php require "../layouts/header.php"; ?>
+<?php require "../../config/config.php"; ?>
+<?php 
+
+    if(!isset($_SESSION['admin_name'])){
+        header("location: ".ADMINURL."/admins/login-admins.php");
+    }
+    
+    if(isset($_GET['id'])){
+
+        $id = $_GET['id'];
+
+        
+        //delete the image
+        $select = $conn->query("SELECT * FROM products WHERE id='$id'");
+        $select->execute();
+
+        $image = $select->fetch(PDO::FETCH_OBJ);
+
+        unlink("images/".$image->image."");
+        
+        
+        $delete = $conn->query("DELETE FROM products WHERE id='$id'");
+        $delete->execute();
+
+        header("location: show-products.php");
+    }
